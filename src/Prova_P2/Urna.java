73dc21ed7d4630ba5f249dc8644e6cb2d6777df7
jhenriqueax <jhenriquealmeidaxavier@hotmail.com>
2022-07-01 11:49:10 -0300

    voto

--- src/Prova_P2/Urna.java
+++ src/Prova_P2/Urna.java
@@ -13,10 +13,7 @@ public class Urna {
 	private Candidato[] candidatos;
 
 	private int contaVotos;
-	
-	
-	
-	
+
 	public Urna(int qntEleitores, int qntCandidatos) {
 		this.qntEleitores = qntEleitores;
 		this.qntCandidatos = qntCandidatos;
@@ -24,17 +21,14 @@ public class Urna {
 		this.eleitores = new Eleitor[this.qntEleitores];
 		this.candidatos = new Candidato[this.qntCandidatos + 2]; // +2 para branco e nulo
 
-		this.candidatos[this.qntCandidatos -1] = new Candidato("nulo"); // -1 para salva na ultima posição do array
-		this.candidatos[this.qntCandidatos -2] = new Candidato("branco"); // -2 para salva na penultima posição do array
-		
+		this.candidatos[this.qntCandidatos - 1] = new Candidato("nulo"); // -1 para salva na ultima posição do array
+		this.candidatos[this.qntCandidatos - 2] = new Candidato("branco"); // -2 para salva na penultima posição do
+																			// array
+
 		this.statusVotacao = 1;
 		this.contaVotos = 0;
 	}
 
-	
-
-	
-	
 	public void cadastrarEleitor(String nome, String cpf) {
 		for (int i = 0; i < eleitores.length; i++) {
 			if (eleitores[i] != null) {
@@ -44,8 +38,6 @@ public class Urna {
 			}
 		}
 	}
-
-
 
 	private boolean comparaCanditado(String nome) {
 
@@ -80,164 +72,163 @@ public class Urna {
 		}
 	}
 
-	
-	private boolean todosEleitores(){
-		
+	private boolean todosEleitores() {
+
 		boolean fullEleitores = true;
-		
+
 		for (int i = 0; i < eleitores.length; i++) {
-			if(eleitores[i] == null){
+			if (eleitores[i] == null) {
 				fullEleitores = false;
 				break;
 			}
 		}
-		
+
 		return fullEleitores;
 	}
 
-	
-	
-	private boolean todosCandidatos(){
-		
+	private boolean todosCandidatos() {
+
 		boolean fullCandidatos = true;
-		
+
 		for (int i = 0; i < eleitores.length; i++) {
-			if(eleitores[i] == null){
+			if (eleitores[i] == null) {
 				fullCandidatos = false;
 				break;
 			}
 		}
-		
+
 		return fullCandidatos;
 	}
-	
-	public void iniciarVotacao(){
-		if(statusVotacao == 1) {
-			if(todosCandidatos() == true && todosEleitores() == true){
-				
+
+	public void iniciarVotacao() {
+		if (statusVotacao == 1) {
+			if (todosCandidatos() == true && todosEleitores() == true) {
+
 				for (int i = 0; i < eleitores.length; i++) {
 					eleitores[i].setStatusVotacao(false);
 				}
 				for (int j = 0; j < candidatos.length; j++) {
 					candidatos[j].zeraVotos();
 				}
-				
-				this.statusVotacao = 2;
-				
-			}
-			
-		}
-		
-	}
-	
-	
-	
-	private int pegaEleitor(String cpfEleitor){
-		
-		int posicaoEleitor = 0;
-			
-			Eleitor pegaEleitor = new Eleitor("nome", cpfEleitor);
-			
-			for (int i = 0; i < eleitores.length; i++) {
-				if(eleitores[i].equals(pegaEleitor)){
-					posicaoEleitor = i;
-					break;
-				}
-			}
-		
-		return posicaoEleitor;
-		
-	}
-	
-	private int pegaCandidato(String nomeCandidato){
-	
-		int posicaoCandidato = 0;
-		
-			Candidato pegaCandidato = new Candidato(nomeCandidato);
-			
-			for (int j = 0; j < candidatos.length; j++) {
-				if(candidatos[j].equals(pegaCandidato)){
-					posicaoCandidato = j;
-					break;
-				}
-			}
-		
-			return posicaoCandidato;
-		}
-		
-		
-	
 
-	
-	public void RegistrarVotos(String cpfEleitor, String nomeCandidato) {
-	
+				this.statusVotacao = 2;
+
+			}
+
+		}
+
+	}
+
+	private int pegaEleitor(String cpfEleitor) {
+
+		int posicaoEleitor = 404;
+
+		Eleitor pegaEleitor = new Eleitor("nome", cpfEleitor);
+
+		for (int i = 0; i < eleitores.length; i++) {
+			if (eleitores[i].equals(pegaEleitor)) {
+				posicaoEleitor = i;
+				break;
+			}
+		}
+
+		return posicaoEleitor;
+
+	}
+
+	private int pegaCandidato(String nomeCandidato) {
+
+		int posicaoCandidato = 404;
+
+		Candidato pegaCandidato = new Candidato(nomeCandidato);
+
+		for (int j = 0; j < candidatos.length; j++) {
+			if (candidatos[j].equals(pegaCandidato)) {
+				posicaoCandidato = j;
+				break;
+			}
+		}
+
+		return posicaoCandidato;
+	}
+
+	public String RegistrarVotos(String cpfEleitor, String nomeCandidato) {
+
+		String retorno = "";
+
 		int posicaoEleitor;
 		int posicaoCandidato;
-		
-		if(statusVotacao == 2) {
-			
+
+		if (statusVotacao == 2) {
+
 			posicaoEleitor = pegaEleitor(cpfEleitor);
 			posicaoCandidato = pegaCandidato(nomeCandidato);
-			
-			eleitores[posicaoEleitor].setStatusVotacao(true);
-			candidatos[posicaoCandidato].SetVotos();
-			
-			contaVotos += 1;
+
+			if (posicaoCandidato == 404) {
+				retorno = "Candidato não cadastrado - Voto não Cadastrado";
+			} else {
+				if (eleitores[posicaoEleitor].getStatus() == true) {
+					retorno = "Eleitor já votou anteriormente - Voto desconsiderado";
+				}
+
+				else {
+					eleitores[posicaoEleitor].setStatusVotacao(true);
+					candidatos[posicaoCandidato].SetVotos();
+					contaVotos += 1;
+					retorno = "Voto cadastrado corretamente";
 				}
 			}
-		
-	public String toStringUrna(){
-		
+		}
+		return retorno;
+	}
+
+	public String toStringUrna() {
+
 		String status = "";
-		
-		if(statusVotacao == 1){
-			status = "NÃO INICIADA";	
-		}if(statusVotacao == 2){
+
+		if (statusVotacao == 1) {
+			status = "NÃO INICIADA";
+		}
+		if (statusVotacao == 2) {
 			status = "EM ANDAMENTO";
-		}if(statusVotacao == 3){
+		}
+		if (statusVotacao == 3) {
 			status = "ENCERRADA";
 		}
-		
-		
+
 		String retorno = "Quantidade de Eleitores Permitidos: " + qntEleitores + " - "
-				+ "Quantidade de Candidatos Permitidos: " + qntCandidatos + " - " +
-				status + "Quantidade de Votos Depositados: " + contaVotos; 
-				
-				return retorno;
-		
+				+ "Quantidade de Candidatos Permitidos: " + qntCandidatos + " - " + status
+				+ "Quantidade de Votos Depositados: " + contaVotos;
+
+		return retorno;
+
 	}
-	
-	
-	public String toStringIniciaVotacao(){
-		
+
+	public String toStringIniciaVotacao() {
+
 		String candidatosRetorno = "";
 		String eleitoresRetorno = "";
-		
-	
+
 		for (int i = 0; i < candidatos.length; i++) {
-			if(candidatos != null){
+			if (candidatos != null) {
 				candidatosRetorno += candidatos[i].toStringCandidato() + "\n";
 			}
-			
+
 		}
-		
+
 		for (int j = 0; j < eleitores.length; j++) {
-			if(eleitores != null){
+			if (eleitores != null) {
 				eleitoresRetorno += eleitores[j].toStringEleitor() + "\n";
 			}
-			
+
 		}
-		
-		String retorno = "-----------"  + "Candidatos" + "-----------" + "\n" + 
-		candidatosRetorno + "\n" + "-----------"  + "Eleitores" + "-----------" +
-			eleitoresRetorno;
-		
+
+		String retorno = "-----------" + "Candidatos" + "-----------" + "\n" + candidatosRetorno + "\n" + "-----------"
+				+ "Eleitores" + "-----------" + eleitoresRetorno;
+
 		return retorno;
-		
+
 	}
-
-
-
 
 	public void encerrar() {
 		this.statusVotacao = 3;
